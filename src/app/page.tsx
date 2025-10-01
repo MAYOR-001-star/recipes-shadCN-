2e5f375bf2f4aa2ebb2a69782ed51cdb45f076ad
baruwa abdul-azeez mayowa <mayorkunabdulazeez01@gmail.com>
2025-10-01 14:35:46 +0100

    shadcn learning

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+interface Recipe {
+  id: string;
+  title: string;
+  image: string;
+  time: number;
+  description: string;
+  vegan: boolean;
+}
+
+async function getRecipes(): Promise<Recipe[]> {
+  const result = await fetch("http://localhost:4000/recipes");
+  return result.json();
+}
+
+export default async function Home() {
+  const recipes = await getRecipes();
+  return (
+    <main>
+      <div className="grid grid-cols-3 gap-8">
+        {recipes.map((recipe) => {})}
+      </div>
+    </main>
+  );
+}
